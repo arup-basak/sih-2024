32c6e6468b6e698e49beb08176061cbb6c1f2be9
Arup Basak <email.arupbasak@gmail.com>
2024-09-04 00:09:25 +0530

    Initial Setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Surface } from "react-native-paper";
+
+export default function Index() {
+  return (
+    <Surface>
+      <View></View>
+    </Surface>
+  );
+}
