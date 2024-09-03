8575a3910b72ed14c34989ec37f16c896a584696
Atif Ali <atif276ali@gmail.com>
2024-09-04 03:03:24 +0530

    added login page

--- app/(auth)/login/index.tsx
+++ app/(auth)/login/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, Image } from "react-native";
 import React from "react";
 import Logo from "@/components/Logo";
 import { Button, Surface, TextInput } from "react-native-paper";
@@ -10,16 +10,38 @@ export default function index() {
   const handleSignInGoogle = () => {};
   return (
     <Surface style={styles.container}>
-      <View>
-        <Text>Welcome To</Text>
+      <View className="w-full flex justify-center items-center py-24 bg-[#FFEEBD]">
+        <Text className="text-2xl font-medium mb-4">Welcome To</Text>
         <Logo />
       </View>
-      <View>
-        <Text>Sign In or Create an Account</Text>
+      <View className="bg-[#4CAF50] h-full rounded-t-3xl py-20 px-10">
+        <Text className="text-white text-2xl font-semibold mb-8">
+          Sign In or Create an Account
+        </Text>
         <TextInput label="Enter Mobile No" mode="outlined" />
+        <Button
+          mode="contained"
+          onPress={handleSignIn}
+          buttonColor="#FFD814"
+          textColor="black"
+          className="my-4"
+        >
+          Continue
+        </Button>
         <Button onPress={handleSignIn}>Sign In</Button>
 
-        <Pressable onPress={handleSignInGoogle}>Sign In With Google</Pressable>
+        <Text
+          className="bg-white w-full p-3 font-semibold rounded-lg text-center"
+          onPress={handleSignInGoogle}
+        >
+          Sign In With Google
+        </Text>
+        <Text
+          className="bg-white w-full p-3 font-semibold rounded-lg text-center mt-4"
+          onPress={handleSignInGoogle}
+        >
+          Sign In With Apple
+        </Text>
       </View>
     </Surface>
   );
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 export default function Logo() {
   return (
-    <View>
-      <Text>HarvestLink</Text>
-    </View>
-  )
+    <Text className="text-5xl font-bold text-black pb-4">
+      Harvest
+      <Text className="text-[#4CAF50]">Link</Text>
+    </Text>
+  );
 }
